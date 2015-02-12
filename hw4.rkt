aa7fa95296fdf35bd410c2f5df4f446e69bcec35
Luuk Veenis <luukveenis@shaw.ca>
2015-02-12 15:28:21 -0800

    Implement list-nth-mod function

--- hw4.rkt
+++ hw4.rkt
@@ -14,3 +14,8 @@
   (map (lambda (s)
          (string-append s suffix))
        xs))
+
+(define (list-nth-mod xs n)
+  (cond [(< n 0) (error "list-nth-mod: negative number")]
+        [(null? xs) (error "list-nth-mod: empty list")]
+        [#t (car (list-tail xs (remainder n (length xs))))]))
